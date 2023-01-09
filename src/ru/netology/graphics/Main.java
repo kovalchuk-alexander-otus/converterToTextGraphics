1f6df3aa0226090d38133fa2ba42d4b4510bdc6e
alexander.kovalchuk <alexander.kovalchuk.aka.maki@gmail.com>
2023-01-09 03:48:00 +0300

    курсовая работа - Конвертер изображения в текстовую графику

--- src/ru/netology/graphics/Main.java
+++ src/ru/netology/graphics/Main.java
@@ -1,21 +1,26 @@
 package ru.netology.graphics;
 
 import ru.netology.graphics.image.TextGraphicsConverter;
+import ru.netology.graphics.image.TextGraphicsConverterImpl;
 import ru.netology.graphics.server.GServer;
-
-import java.io.File;
-import java.io.PrintWriter;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        TextGraphicsConverter converter = null; // Создайте тут объект вашего класса конвертера
+        TextGraphicsConverter converter = new TextGraphicsConverterImpl(); // Создайте тут объект вашего класса конвертера
 
         GServer server = new GServer(converter); // Создаём объект сервера
         server.start(); // Запускаем
 
-        // Или то же, но с выводом на экран:
-        //String url = "https://raw.githubusercontent.com/netology-code/java-diplom/main/pics/simple-test.png";
-        //String imgTxt = converter.convert(url);
-        //System.out.println(imgTxt);
+//          Или то же, но с выводом на экран:
+//        converter.setMaxRatio(1.5);
+//        converter.setMaxWidth(200);
+//        converter.setMaxHeight(200);
+//
+//        TextColorSchema schema = new TextColorSchemaImpl(new char[]{'@', '#', '$', '%', '&', 'Q', 'q', 'i', '^', '*', '_', ' '});
+//        converter.setTextColorSchema(schema);
+//
+//        String url = "https://raw.githubusercontent.com/netology-code/java-diplom/main/pics/simple-test.png";
+//        String imgTxt = converter.convert(url);
+//        System.out.println(imgTxt);
     }
 }
